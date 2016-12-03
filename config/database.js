fec84dec74dcfd5469e43b0df0ac54b6266ba3c6
tchar989 <tkc218@lehigh.edu>
2016-12-03 01:57:07 -0500

    first commit

---
+++ config/database.js
@@ -0,0 +1,3 @@
+module.exports = {
+    'url'   : 'mongodb://172.31.43.6/mydatabase' // TODO: put the path to your database, such as 'mongodb://100.2.5.6/mydatabase'
+};
---
+++ models/users.js
@@ -0,0 +1,38 @@
+var mongoose = require('mongoose');
+var bcrypt   = require('bcrypt-nodejs');
+
+// define the schema for our user model
+
+var trans = {
+    date : Date,
+    catID : Number,
+    amount : Number
+};
+
+var userSchema = mongoose.Schema({
+    // data for local user accounts
+    userID : Number,
+    local            : {
+        email        : String,
+        password     : String,
+    },
+    // data for Google OAuth accounts
+    google           : {
+        id           : String,
+        token        : String,
+        email        : String,
+        name         : String,
+    },
+    transactions     : [trans]
+});
+
+// extend schema with methods to generate hash and check if password valid
+userSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+userSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.local.password);
+};
+
+// create the model and expose it to our app
+module.exports = mongoose.model('User', userSchema);

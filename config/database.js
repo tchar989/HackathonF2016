df43df6d0763626ae60d628a30952f4ea4ed5625
tchar989 <tkc218@lehigh.edu>
2016-12-03 03:55:27 -0500

    changes to db url

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-    'url'   : 'mongodb://35.162.151.164/cool_db' // TODO: put the path to your database, such as 'mongodb://100.2.5.6/mydatabase'
+    'url'   : 'mongodb://Tim:Hackathon!@35.162.151.164/cool_db' // TODO: put the path to your database, such as 'mongodb://100.2.5.6/mydatabase'
 };
